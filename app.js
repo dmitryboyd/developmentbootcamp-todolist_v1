e3a50d4032282ff5c5af127038c26707b6fda83a
dimabrown <ddimabrown@gmail.com>
2021-03-14 18:27:50 +0200

    complete to add new Item

--- app.js
+++ app.js
@@ -1,18 +1,37 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const ejs = require("ejs")
+const ejs = require("ejs");
 const app = express();
 
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+let items = ["Buy Food", "Cook food", "Eat Food"];
+
 app.get("/", (req, res) => {
-
     let today = new Date();
-    let currentDay = today.getDay();
-
-    if (currentDay === 6 || currentDay === 0){
-        res.sendFile(__dirname + "/index.html")
-    } else {
-        res.write("work")
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
     }
+    let day =  today.toLocaleDateString("en-US", options);
+    // let currentDay = today.getDay();
+    // let day = "";
+    // let dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    //
+    // day = currentDay === 6 || currentDay === 0 ? dayNames[currentDay] : dayNames[currentDay];
+    res.render("list", {
+        kindOfDay: day,
+        newListItems: items
+    });
+});
+
+app.post("/", (req, res) => {
+    item = req.body.newItem;
+    items.push(item);
+
+    res.redirect("/")
 });
 
 
