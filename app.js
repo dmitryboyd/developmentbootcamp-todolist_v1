0f251b198de1155f58e7e4aee81c5ccb0b25b85d
dimabrown <ddimabrown@gmail.com>
2021-03-20 21:37:12 +0200

    add 3 item for default Items list

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true}, { useUnifiedTopology: true });
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
 const itemsSchema = {
     name: String
@@ -15,16 +15,28 @@ const itemsSchema = {
 
 const Item = mongoose.model("Item", itemsSchema);
 
-const item = new Item({
-    name: "Eat Food"
+const item1 = new Item({
+    name: "Welcome to your todolist!"
+});
+
+const item2 = new Item({
+    name: "Hit the + button to add a new item."
 })
 
-// item.save()
+const item3 = new Item({
+    name: "<-- Hit this to delete an item."
+})
+
+const defaultItems = [item1, item2, item3];
+
+// Item.insertMany([item1, item2, item3], (err) => {
+//     err ? console.log(err) : console.log("OK")
+// })
 
 app.get("/", (req, res) => {
     res.render("list", {
         listTitle: "Today",
-        newListItems: items
+        newListItems: defaultItems
     });
 });
 
